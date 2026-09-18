aa226d6fe847af9313a186ab0c3fe4cdc7623ec5
pallavi-srinivas <pallvisrinivas@gmail.com>
2026-09-18 15:31:24 -0400

    Enhance screen_clock.py to display current time
    Added import for strftime and sleep from time module. Updated the clock display to show current time.

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,4 +1,5 @@
 import time
+from time import strftime, sleep
 import subprocess
 import digitalio
 import board
@@ -64,7 +65,10 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=400)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
+    current_time = strftime("%m/%d/%Y %H:%M:%S") 
+    y = top
+    draw.text((x, y), current_time, font=font, fill="#FFFFFF")
 
     # Display image.
     disp.image(image, rotation)
